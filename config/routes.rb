f62bb5130f71a7cf75c43b01659004f1e9ddf018
Alicia Chao <aliciachao@gmail.com>
2020-03-17 12:56:08 -0700

    routes, schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :listings
+  # get 'listings/twenty', to: 'listings#twenty'
+  resources :tasks
+  resources :trackers
+  resources :ul_trackers
+  resources :users
+  # post 'users/login', to: 'users#login'
+  resources :users_listings
 end
