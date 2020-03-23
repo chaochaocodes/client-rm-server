97c7b5520146f5a533826a253640bbad99fad46d
Alicia Chao <aliciachao@gmail.com>
2020-03-22 22:05:01 -0700

    auth: app and auth controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
+
   get 'listings/api', to: 'listings#api_call'
   get 'listings/twenty', to: 'listings#twenty'
-  resources :listings
+  resources :listings 
   resources :tasks
   resources :trackers
   resources :ul_trackers
-  post '/login', to: 'auth#create'
-  get '/profile', to: 'users#profile'
   resources :users
   resources :users_listings
 end
