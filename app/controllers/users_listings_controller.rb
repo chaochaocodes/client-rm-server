7e76f73469aed20847515870ab793638eb5fe02b
github username <github email address>
2020-04-18 00:07:30 -0700

    render saved listings

--- app/controllers/users_listings_controller.rb
+++ app/controllers/users_listings_controller.rb
@@ -1,3 +1,22 @@
 class UsersListingsController < ApplicationController
+  # skip_before_action :authorized, only: [:save_listing]
 
+    def index
+      users_listings = UsersListing.all 
+      render json: users_listings
+    end
+
+    def save_listing
+        # byebug
+        listing = Listing.find_or_create_by(params[:listing][:result].permit!)
+        user = User.find(params[:user_id])
+        user.listings << listing
+        render json: user.listings
+    end
+
+    # def show
+    #     users_listing = UsersListing.where(user_id: params[:user_id])
+    #     render json: users_listing
+    # end
+    
 end
